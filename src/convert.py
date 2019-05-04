6259c4dcf6b7e3d180ac20526616240f2f46afbd
Domingo Manubens-Gil <mingu_mg@yahoo.es>
2019-05-04 19:58:29 +0200

    fix decimals

--- src/convert.py
+++ src/convert.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-import glob
 from dateutil import rrule
 from datetime import datetime, timedelta
 
@@ -12,7 +11,7 @@ df_from_each_file = []
 
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
      filename = dt.strftime('%Y%m') + ".csv"
-     df = pd.read_csv(filename, delim_whitespace=True, names=['Data','Temperatura Màx.','Temperatura Mín.','Pressió Màx.','Pressió Mín.','Humitat(%) Màx.','Humitat(%) Mín.','Anemòmetre Dir.','Anemòmetre Km/h.','Pluja l/m2'])
+     df = pd.read_csv(filename, delim_whitespace=True, decimal=',', names=['Data','Temperatura Màx.','Temperatura Mín.','Pressió Màx.','Pressió Mín.','Humitat(%) Màx.','Humitat(%) Mín.','Anemòmetre Dir.','Anemòmetre Km/h.','Pluja l/m2'])
      df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
      df_from_each_file.append(df)
 
